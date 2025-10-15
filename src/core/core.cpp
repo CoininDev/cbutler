d9debc9d66d4d0d8e985ee109cd4b6cafcca4e49
CoininDev <gabr.batz@gmail.com>
2025-10-14 21:35:04 -0300

    a

---
+++ src/core/core.cpp
@@ -0,0 +1,1 @@
+#include <tiny-process-library/process.hpp>
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <tiny-process-library/process.hpp>
+
 
 int main() {
-    std::cout << "Ola Mundo!" << std::endl;
+    TinyProcessLib::Process process(
+        "mkdir teste",
+        "",
+        [](const char *bytes, size_t n) {
+            std::cout << "output from stdout: " << bytes << std::endl;
+    });
+    auto exit_status = process.get_exit_status();
+    std::cout << exit_status << std::endl;
+    return 0;
 }
